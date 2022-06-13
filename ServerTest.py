d8cc44998db63647c8fdad0f806f0d65e0d00ef8
Stelian Grozev <47190564+NqkoiNqkojsi@users.noreply.github.com>
2022-06-13 14:58:10 +0300

    made the camera feed show in browser

--- ServerTest.py
+++ ServerTest.py
@@ -1,27 +1,28 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-
-
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>Simple Server</title></head>", "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is Yang's Mac Mini.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
-
-
+#Import necessary libraries
+from flask import Flask, render_template, Response
+import cv2
+#Initialize the Flask app
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+'''
+for ip camera use - rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' 
+for local webcam use cv2.VideoCapture(0)
+'''
+def gen_frames():  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 if __name__ == "__main__":
-    webServer = HTTPServer(('localhost', 8080), MyServer)
-    print("starting server")
-    print(webServer.server_address)
-    print(webServer.server_name)
-    print(webServer.socket)
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
+    app.run(debug=True) 

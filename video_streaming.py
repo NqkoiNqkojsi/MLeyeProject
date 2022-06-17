d39efaed9e51014aeab365d0eaf8ff1514fb971f
Stelian Grozev <tanya_dzh@abv.bg>
2022-06-17 03:50:50 +0300

    last minute ajustments

--- video_streaming.py
+++ video_streaming.py
@@ -3,26 +3,19 @@
 import cv2
 import FaceTest as fc
 #def Is_Active():
-
-# define a video capture object
-vid = cv2.VideoCapture(0)
+def Stream():
+    # define a video capture object
+    vid = cv2.VideoCapture(0)
   
-while(True):
+    while(True):
       
-    # Capture the video frame
-    # by frame
-    ret, frame = vid.read()
-  
-    # Display the resulting frame
-    fc.picture_anal(frame)
+        # Capture the video frame
+        # by frame
+        ret, frame = vid.read()
+        # Display the resulting frame
+        return fc.picture_anal(frame)
       
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-  
-# After the loop release the cap object
-vid.release()
-# Destroy all the windows
-cv2.destroyAllWindows()
+    # After the loop release the cap object
+    vid.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()

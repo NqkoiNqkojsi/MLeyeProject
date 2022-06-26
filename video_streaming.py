cab5723109439515e263860eac455946f0285db3
Stelian Grozev <47190564+NqkoiNqkojsi@users.noreply.github.com>
2022-06-26 18:41:48 +0300

    Update video_streaming.py

--- video_streaming.py
+++ video_streaming.py
@@ -4,45 +4,51 @@ import cv2
 import FaceTest as fc
 import time
 import requests
-
+ 
 ip="192.168.1.10:5000"
-
+ 
 def Stream():
     # define a video capture object
     vid = cv2.VideoCapture(0)
-    while(True):
-        ret, frame = vid.read()
-        return fc.picture_anal(frame)
-
-
+    ret, frame = vid.read()
+    return fc.picture_anal(frame)
+ 
+ 
 def Setup():
     global isActive
     isActive=False
-
-
-
+ 
+ 
+ 
 def SetIp(ipAddr):
     ip=ipAddr
-
-async def Main_Run(mode, ip):
+ 
+def Main_Run(mode, ip):
     print("ip="+ip)
+    isAsleep=False
     isActive=True
     iter=0
-    state=[1,1,1,1,1,1,1,1,1,1,1]
+    state=[1,1,1,1,1,1,1,1]
     while(True):
-        for x in range(0, 10):
+        for x in range(0, 7):
             state[x]=state[x+1]
-        state[10]=Stream()
-        if state.count(0)>7:
+        state[7]=Stream()
+        print(state)
+        if isAsleep==True and state[7]==1:
+            state=[0,0,0,1,1,1,1,1]
+            isAsleep=False
+        if state.count(0)>4:
+            isAsleep=True
             try:
                 if mode==0:
                     r = requests.get(str("http://"+ip+"/taze"), auth=('user', 'pass'))
                 else:
                     r = requests.get(str("http://"+ip+"/vibrate"), auth=('user', 'pass'))
-                await asyncio.sleep(0.5)
+                time.sleep(1)
             except:
+                
                 return
-
+ 
 def Test_run(mode):
     isActive=True
     iter=0
@@ -57,4 +63,4 @@ def Test_run(mode):
             else:
                 r = requests.get(str("http://"+ip+"/vibrate"), auth=('user', 'pass'))
         time.sleep(0.5)
-#Test_run(1)
+Main_Run(1,ip)
